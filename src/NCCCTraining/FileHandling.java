a876f6b6caff8804fc75ec32698dff177aefc19b
colyb <colyb@10.104.144.216>
2018-11-15 12:45:56 -0800

    method defining

--- src/NCCCTraining/FileHandling.java
+++ src/NCCCTraining/FileHandling.java
@@ -1,5 +1,13 @@
 package NCCCTraining;
 
+import java.io.File;
+
 public class FileHandling {
+	public static void OpenFile(String file) {
+		
+	}
 	
+	public static void getFileSize(File file) {
+		
+	}
 }
--- src/NCCCTraining/NCCCAideTraining.java
+++ src/NCCCTraining/NCCCAideTraining.java
@@ -3,7 +3,7 @@ package NCCCTraining;
 public class NCCCAideTraining {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
